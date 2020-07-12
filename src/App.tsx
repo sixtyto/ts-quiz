0f49abf7c2eaed1ba40a3ed6edb617290a48f038
Piotr Czajkowski <sixtyto@gmail.com>
2020-07-12 09:06:57 +0200

    added states

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import QuestionCard from './components/QuestionCard';
 
+const TOTAL_QUESTIONS = 10;
+
 const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
   const startQuiz = async () => {};
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
   const nextQuestion = () => {};
@@ -9,9 +17,16 @@ const App = () => {
     <div>
       <h1>Typescript Quiz Game</h1>
       <button onClick={startQuiz}>Start</button>
-      <p>Score:</p>
+      <p>Score: {score}</p>
       <p>Loading Question...</p>
-      <QuestionCard />
+      <QuestionCard
+        question={questions[number]}
+        answers={questions[number]}
+        callback={checkAnswer}
+        userAnswer={userAnswers ? userAnswers[number] : null}
+        questionNumber={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+      />
       <button onClick={nextQuestion}>Next Question</button>
     </div>
   );
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
 
-const QuestionCard = () => {
-  return <div>Question Card</div>;
+type Props = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: any;
+  questionNumber: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNumber,
+  totalQuestions,
+}) => {
+  return (
+    <div>
+      <p>
+        Question: {questionNumber} / {totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }} />
+      <div>
+        {answers.map(answer => (
+          <div>
+            <button disabled={userAnswer} onClick={callback}>
+              <span dangerouslySetInnerHTML={{ __html: answer }} />
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default QuestionCard;
