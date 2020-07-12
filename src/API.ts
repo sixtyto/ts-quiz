88a0917ee77c8bb9bad8207d08c4e274934d0e04
Piotr Czajkowski <sixtyto@gmail.com>
2020-07-12 11:01:58 +0200

    added answer data

--- src/API.ts
+++ src/API.ts
@@ -1,3 +1,14 @@
+import { shuffleArray } from './utilities';
+
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+};
+export type QuestionState = Question & { answers: string[] };
 export enum Difficulty {
   EASY = 'easy',
   MEDIUM = 'medium',
@@ -9,5 +20,11 @@ export const fetchQuizQuestions = async (
 ) => {
   const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
   const data = await await (await fetch(endpoint)).json();
-  console.log(data);
+  return data.results.map((question: Question) => ({
+    ...question,
+    answers: shuffleArray([
+      ...question.incorrect_answers,
+      question.correct_answer,
+    ]),
+  }));
 };
